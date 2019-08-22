eede41d9af213ea62ecc17a075f1d0eb9fdcd866
chopachopk <monkeyslutz@yandex.ru>
2019-08-23 02:05:34 +0300

    Поправила стилевые ошибки gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -57,9 +57,9 @@ gulp.task("html", function () {
 gulp.task("images", function () {
   return gulp.src("source/img/**/*.{png,jpg,svg}")
     .pipe(imagemin([
-       imagemin.optipng({optimizationLevel: 3}),
-       imagemin.jpegtran({progressive: true}),
-       imagemin.svgo()
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
     ]))
     .pipe(gulp.dest("source/img"));
 });
