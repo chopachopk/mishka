6ff4d20b3cbe96e5964d1c4b0ed64e3455a013c6
chopachopk <monkeyslutz@yandex.ru>
2019-08-27 22:43:44 +0300

    Реализовала открытие попапа

--- source/js/script.js
+++ source/js/script.js
@@ -15,3 +15,27 @@ navToggle.addEventListener('click', function() {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+// Попап для заказа
+var link = document.querySelector('.button--order');
+var popup = document.querySelector('.popup');
+var overlay = document.querySelector('.overlay');
+// Показать попап
+link.addEventListener('click', function (evt)  {
+    evt.preventDefault();
+    popup.classList.add('popup-show');
+});
+// Закрыть попап по клику на overlay
+overlay.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  popup.classList.remove('popup-show');
+});
+// Закрыть попап по esc
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode == 27) {
+    if (popup.classList.contains('popup-show')) {
+      evt.preventDefault();
+      popup.classList.remove('popup-show');
+    }
+  }
+});
